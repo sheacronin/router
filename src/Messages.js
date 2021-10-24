6577a9500031a852e672e390e93a0cd70f57aa1a
Shea Cronin <shealcronin@gmail.com>
2021-10-24 16:54:26 -0400

    Add Messages component

---
+++ src/Messages.js
@@ -0,0 +1,9 @@
+const Messages = () => {
+    return (
+        <div>
+            <h1>Hello from Messages</h1>
+        </div>
+    );
+};
+
+export default Messages;
--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
+import Messages from './Messages';
 import Profile from './Profile';
 
 const Routes = () => {
@@ -8,6 +9,7 @@ const Routes = () => {
             <Switch>
                 <Route exact path="/" component={App} />
                 <Route exact path="/profile" component={Profile} />
+                <Route exact path="/messages" component={Messages} />
             </Switch>
         </BrowserRouter>
     );
