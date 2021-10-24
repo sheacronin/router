db7191b0e433b08f45878ad1ba65032945e6c546
Shea Cronin <shealcronin@gmail.com>
2021-10-24 16:42:56 -0400

    Set up Routes

---
+++ src/Routes.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import App from './App';
+import Profile from './Profile';
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" component={App} />
+                <Route path="/profile" component={Profile} />
+            </Switch>
+        </BrowserRouter>
+    );
+};
+
+export default Routes;
